78b5aba4e102616f16644c8499ce5b7a38e9c380
angelODev26 <angel0626199664@gmail.com>
2025-07-24 19:12:03 -0500

    feat: add exception handling for NoSuchElementException in ValidationExceptionHandler

--- src/main/java/com/lms/api/exceptions/ValidationExceptionHandler.java
+++ src/main/java/com/lms/api/exceptions/ValidationExceptionHandler.java
@@ -2,6 +2,7 @@ package com.lms.api.exceptions;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.lms.api.payload.ApiResponse;
+
 @ControllerAdvice
 public class ValidationExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
 
@@ -24,6 +27,12 @@ public class ValidationExceptionHandler {
         });
 
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ApiResponse<Void>> handleNoSuchElementException(NoSuchElementException ex) {
+        ApiResponse<Void> response = new ApiResponse<>(false, ex.getMessage(), null);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
     }
 
 }
